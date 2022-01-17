efcb6220ce0b8f79f0da328a3747b2c8ad54b226
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-01-17 15:50:21 -0300

    05 criaçao da funçao createStudent atendendo aos testes

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const estudante = { name: '', feedback: () => 'Eita pessoa boa!' }; 
+  estudante.name = name;
+  return estudante;
+};
 
 module.exports = createStudent;
